5a19d175d7b99ecfa8b0fced28139f3ce321eaec
ruslanbek95 <kg_tulit@inbox.ru>
2023-07-14 15:06:42 +0300

    comit

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,14 @@ public class Main {
     public static void main(String[] args) {
 
         Group group = new Group(312, "words", new int[]{12, 3, 12, 45, 5, 323, 4});
-        Group san = group;
-        int san1 = group.getNumber();
+
         for (int i = 0; i < group.getMas().length; i++) {
-            if (san == group && san1 == group.getNumber() &&) {
-                san = group;
-                san1 = group.getNumber();
-                System.out.println(san.getMas()[i]);
+            System.out.print(group.getMas()[i]+" ");
             }
-        }
-        System.out.println(group.getWords() + "\n" + group.getNumber());
+        System.out.println("\n"+group.getNumber()+"\n"+group.getWords());
     }
 }
+
 
 
 
